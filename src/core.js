dd09453404d7b9b9a9d6357829cd5c65f4de8dff
Muhammad Habib Rohman <mhrohman@live.com>
2017-02-12 10:44:25 +0700

    separate promise each polyfill

--- src/core.js
+++ src/core.js
@@ -12,16 +12,7 @@ import {
   colorNameToHex,
   isColor
 } from './utils'
-
-Promise.each = function(arr, fn) { // take an array and a function
-  // invalid input
-  if(!arr || !arr.length) return Promise.reject(new Error("Non array passed to each"))
-  // empty case
-  if(arr.length === 0) return Promise.resolve()
-  return arr.reduce(function(prev, cur) {
-    return prev.then(() => fn(cur))
-  }, Promise.resolve())
-}
+import './polyfill'
 
 const _config = new WeakMap()
 
